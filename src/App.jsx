7ec58b383fcf14fa95226cfcdbe7b8b441089ada
GabrielaTumfart <gabrielatumfart@gmail.com>
2026-02-24 12:25:34 +0100

    feat: ora importo il componente Header in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import Header from './components/Header';
+
 export default function App() {
   
 
   return (
     <>
+
+    <Header />
     <h1>BoolFlix</h1>;
+
      
     </>
   )
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+    return (
+      <>
+        <h1>BoolFlix</h1>
+        <div>
+          <input type="text" placeholder="Cerca un film..." />
+          <button>Cerca</button>
+        </div>
+      </>
+    );
+  }
