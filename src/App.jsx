b2555ff43b5feab5699944094271de2b07f88614
GabrielaTumfart <gabrielatumfart@gmail.com>
2026-02-24 12:35:48 +0100

    chore:  per quanto riguarda il search seguo il metodo del Tutor Stefano, copio nel mio progetto e vedo cosa posso urufruire

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,36 @@
+/*Tutor Stefano ci ha disponibillizzato:
+function Component() {
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const loadData = () => {
+    // per alcune api potrebbe essere necessario usare un endpoint diverso
+    // se search è vuoto
+    axios
+      .get(`https://dummyjson.com/products/search?q=${search}`)
+      .then((res) => setData(res.data));
+  };
+
+  // se metto search nelle dipendenze, caricamento per ogni volta che digito, IN SPECIALIZZAZIONE
+  // TECNICHE PER GESTIRLO CORRETTAMENTE (magari la clear up function potrebbe aiutare...)
+  useEffect(loadData, [search]);
+
+  // se NON metto search nelle dipendenze, non faccio fetch quando digito MA! lo delego quando clicco il pulsante
+  useEffect(loadData, []);
+
+  return (
+    <>
+      <input
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <button onClick={loadData}>Search!</button>
+    </>
+  );
+}
+*/
+
 import Header from './components/Header';
 
 export default function App() {
